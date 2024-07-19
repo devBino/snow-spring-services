cda4214ba1f92563e8e8dc8088c9dd1150536c9f
devBino <programemachado@gmail.com>
2024-07-19 20:12:26 -0300

    ci:4 - Implementação de CI para release corrente

---
+++ api-livros/src/test/java/br/com/snowmanlabs/api_livros/configs/TestConfigs.java
@@ -0,0 +1,18 @@
+package br.com.snowmanlabs.api_livros.configs;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("development")
+public class TestConfigs {
+    
+    @Test
+    public void testConfigIsOkParaCI(){
+        assertTrue(true);
+    }
+
+}
