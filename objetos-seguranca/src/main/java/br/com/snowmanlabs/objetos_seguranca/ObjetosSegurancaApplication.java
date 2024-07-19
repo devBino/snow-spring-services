f7d516771cf9950868addcf19b34dd3baf00c58f
devBino <programemachado@gmail.com>
2024-07-19 19:48:04 -0300

    feat:Iniciando sub projetos no repositorio

---
+++ objetos-seguranca/src/main/java/br/com/snowmanlabs/objetos_seguranca/ObjetosSegurancaApplication.java
@@ -0,0 +1,13 @@
+package br.com.snowmanlabs.objetos_seguranca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjetosSegurancaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObjetosSegurancaApplication.class, args);
+	}
+
+}
---
+++ objetos-seguranca/src/test/java/br/com/snowmanlabs/objetos_seguranca/ObjetosSegurancaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.snowmanlabs.objetos_seguranca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjetosSegurancaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
