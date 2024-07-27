abe26382ce700547aa0a1a4d0f534a0aa3fd99fd
devBino <programemachado@gmail.com>
2024-07-27 12:12:38 -0300

    feat:79 - Salvando mudanças locais para priorizar outra atividade

---
+++ servico-email/src/main/java/br/com/snowmanlabs/servico_email/domain/model/MUsuario.java
@@ -0,0 +1,32 @@
+package br.com.snowmanlabs.servico_email.domain.model;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity(name = "tb_usuario")
+public class MUsuario implements Serializable {
+    
+    public static final Long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String email;
+    private String cidade;
+    private String uf;
+    private String statusEnvio;
+    private String obsEnvio;
+
+}
